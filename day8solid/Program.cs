19a86e70d0f86a1b4e80a33e95b42b429165de26
Mohamed_A_Zedan <madoz@DESKTOP-MGKIEQP>
2023-02-02 01:27:25 +0200

    Add project files.

---
+++ day8solid/Program.cs
@@ -0,0 +1,45 @@
+
+using day8solid.Models;
+using day8solid.Reposiotries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace day8solid
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<firmDBcontext>(options =>
+            {
+                options.UseSqlServer("Data Source=.;Initial Catalog=Firm;Integrated Security=True ;TrustServerCertificate=True");
+            });
+
+            var app = builder.Build();
+            //builder.Services.AddSingleton<IemployeeReposiotry, EmployeeReposiotry>();
+            builder.Services.AddScoped<IemployeeReposiotry, EmployeeReposiotry>();
+            //builder.Services.AddTransient<IemployeeReposiotry, EmployeeReposiotry>();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
+
